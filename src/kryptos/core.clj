d1733cf3e34f573e1eb40b05354c261c5b4d8da8
GeistInDerSH <GeistInDerSH@gmail.com>
2024-05-05 15:38:50 -0700

    Make Variables Static
    The k1-4 values and solutions can be static, as they will not be changed by the program

--- src/kryptos/core.clj
+++ src/kryptos/core.clj
@@ -9,15 +9,15 @@
       (filter #(Character/isLetter ^Character %))
       (apply str)))
 
-(def k1 "emufphzlrfaxyusdjkzldkrnshgnfivjyqtquxqbqvyuvlltrevjyqtmkyrdmfd")
-(def k2 "vfpjudeehzwetzyvgwhkkqetgfqjnceggwhkkdqmcpfqzdqmmiagpfxhqrlgtimvmzjanqlvkqedagdvfrpjungeunaqzgzlecgyuxueenjtbjlbqcrtbjdfhrryizetkzemvdufksjhkfwhkuwqlszftihhddduvhdwkbfufpwntdfiycuqzereevldkfezmoqqjlttugsyqpfeunlavidxflggtezfkzbsfdqvgogipufxhhdrkffhqntgpuaecnuvpdjmqclqumunedfqelzzvrrgkffvoeexbdmvpnfqxezlgrednqfmpnzglflpmrjqyalmgnuvpdxvkpdqumebedmhdafmjgznuplgeswjllaetg")
-(def k3 "endyahrohnlsrheocpteoibidyshnaiachtnreyuldsllsllnohsnosmrwxmnetprngatihnrarpeslnneleblpiiacaewmtwnditeenrahcteneudretnhaeoetfolsedtiwenhaeioyteyqheenctaycreiftbrspamhhewenatamategyeerlbteefoasfiotuetuaeotoarmaeertnrtibseddniaahttmstewpieroagriewfebaectddhilceihsitegoeaosddrydloritrklmlehagtdhardpneohmgfmfeuheecdmripfeimehnlssttrtvdohw")
-(def k4 "obkruoxoghulbsolifbbwflrvqqprngkssotwtqsjqssekzzwatjkludiawinfbnypvttmzfpkwgdkzxtjcdigkuhuauekcar")
+(def ^:static k1 "emufphzlrfaxyusdjkzldkrnshgnfivjyqtquxqbqvyuvlltrevjyqtmkyrdmfd")
+(def ^:static k2 "vfpjudeehzwetzyvgwhkkqetgfqjnceggwhkkdqmcpfqzdqmmiagpfxhqrlgtimvmzjanqlvkqedagdvfrpjungeunaqzgzlecgyuxueenjtbjlbqcrtbjdfhrryizetkzemvdufksjhkfwhkuwqlszftihhddduvhdwkbfufpwntdfiycuqzereevldkfezmoqqjlttugsyqpfeunlavidxflggtezfkzbsfdqvgogipufxhhdrkffhqntgpuaecnuvpdjmqclqumunedfqelzzvrrgkffvoeexbdmvpnfqxezlgrednqfmpnzglflpmrjqyalmgnuvpdxvkpdqumebedmhdafmjgznuplgeswjllaetg")
+(def ^:static k3 "endyahrohnlsrheocpteoibidyshnaiachtnreyuldsllsllnohsnosmrwxmnetprngatihnrarpeslnneleblpiiacaewmtwnditeenrahcteneudretnhaeoetfolsedtiwenhaeioyteyqheenctaycreiftbrspamhhewenatamategyeerlbteefoasfiotuetuaeotoarmaeertnrtibseddniaahttmstewpieroagriewfebaectddhilceihsitegoeaosddrydloritrklmlehagtdhardpneohmgfmfeuheecdmripfeimehnlssttrtvdohw")
+(def ^:static k4 "obkruoxoghulbsolifbbwflrvqqprngkssotwtqsjqssekzzwatjkludiawinfbnypvttmzfpkwgdkzxtjcdigkuhuauekcar")
 
-(def k1-solution (solution-format "BETWEEN SUBTLE SHADING AND THE ABSENCE OF LIGHT LIES THE NUANCE OF IQLUSION"))
-(def k2-solution (solution-format "IT WAS TOTALLY INVISIBLE HOWS THAT POSSIBLE ? THEY USED THE EARTHS MAGNETIC FIELD X THE INFORMATION WAS GATHERED AND TRANSMITTED UNDERGRUUND TO AN UNKNOWN LOCATION X DOES LANGLEY KNOW ABOUT THIS ? THEY SHOULD ITS BURIED OUT THERE SOMEWHERE X WHO KNOWS THE EXACT LOCATION ? ONLY WW THIS WAS HIS LAST MESSAGE X THIRTY EIGHT DEGREES FIFTY SEVEN MINUTES SIX POINT FIVE SECONDS NORTH SEVENTY SEVEN DEGREES EIGHT MINUTES FORTY FOUR SECONDS WEST X LAYER TWO"))
-(def k3-solution (solution-format "SLOWLY DESPARATLY SLOWLY THE REMAINS OF PASSAGE DEBRIS THAT ENCUMBERED THE LOWER PART OF THE DOORWAY WAS REMOVED WITH TREMBLING HANDS I MADE A TINY BREACH IN THE UPPER LEFT HAND CORNER AND THEN WIDENING THE HOLE A LITTLE I INSERTED THE CANDLE AND PEERED IN THE HOT AIR ESCAPING FROM THE CHAMBER CAUSED THE FLAME TO FLICKER BUT PRESENTLY DETAILS OF THE ROOM WITHIN EMERGED FROM THE MIST X CAN YOU SEE ANYTHING Q ?"))
-(def k4-solution "¯\\_(ツ)_/¯")
+(def ^:static k1-solution (solution-format "BETWEEN SUBTLE SHADING AND THE ABSENCE OF LIGHT LIES THE NUANCE OF IQLUSION"))
+(def ^:static k2-solution (solution-format "IT WAS TOTALLY INVISIBLE HOWS THAT POSSIBLE ? THEY USED THE EARTHS MAGNETIC FIELD X THE INFORMATION WAS GATHERED AND TRANSMITTED UNDERGRUUND TO AN UNKNOWN LOCATION X DOES LANGLEY KNOW ABOUT THIS ? THEY SHOULD ITS BURIED OUT THERE SOMEWHERE X WHO KNOWS THE EXACT LOCATION ? ONLY WW THIS WAS HIS LAST MESSAGE X THIRTY EIGHT DEGREES FIFTY SEVEN MINUTES SIX POINT FIVE SECONDS NORTH SEVENTY SEVEN DEGREES EIGHT MINUTES FORTY FOUR SECONDS WEST X LAYER TWO"))
+(def ^:static k3-solution (solution-format "SLOWLY DESPARATLY SLOWLY THE REMAINS OF PASSAGE DEBRIS THAT ENCUMBERED THE LOWER PART OF THE DOORWAY WAS REMOVED WITH TREMBLING HANDS I MADE A TINY BREACH IN THE UPPER LEFT HAND CORNER AND THEN WIDENING THE HOLE A LITTLE I INSERTED THE CANDLE AND PEERED IN THE HOT AIR ESCAPING FROM THE CHAMBER CAUSED THE FLAME TO FLICKER BUT PRESENTLY DETAILS OF THE ROOM WITHIN EMERGED FROM THE MIST X CAN YOU SEE ANYTHING Q ?"))
+(def ^:static k4-solution "¯\\_(ツ)_/¯")
 
 (defn get-solutions []
   (let [key "kryptos"
