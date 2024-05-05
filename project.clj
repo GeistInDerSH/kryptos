8a2316bda1eb515d3918987e868ab281a109ade8
GeistInDerSH <GeistInDerSH@gmail.com>
2024-05-05 16:34:05 -0700

    Version Bump to Clojure 1.11.3

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject kryptos "0.1.0-SNAPSHOT"
   :description "Kryptos"
   :main kryptos.core
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.3"]]
   :repl-options {:init-ns kryptos.core}
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
