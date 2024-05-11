98144cba23c766a86caa10c3ca9a46de1d46d1c1
GeistInDerSH <GeistInDerSH@gmail.com>
2024-05-10 19:45:38 -0700

    Update Project File
    Update the project file with Git Revision, Built-On.
    Add License information.
    Include addition profiles.

--- project.clj
+++ project.clj
@@ -1,7 +1,18 @@
-(defproject kryptos "0.1.0-SNAPSHOT"
+(defproject kryptos "0.1.0"
   :description "Kryptos"
   :main kryptos.core
+  :aot :all
+  :global-vars {*warn-on-reflection* true}
+  :license {:name "Eclipse Public License - v 2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :offline? true
   :dependencies [[org.clojure/clojure "1.11.3"]]
   :repl-options {:init-ns kryptos.core}
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :repl {:plugins [[nrepl "1.1.1"]]}}
+  :manifest {"Revision" ~(:out (clojure.java.shell/sh "git" "log" "-n1" "--pretty=format:%h"))
+             "Built-On" ~(-> "America/Chicago"
+                             (java.time.ZoneId/of)
+                             (java.time.OffsetDateTime/now)
+                             str)})
